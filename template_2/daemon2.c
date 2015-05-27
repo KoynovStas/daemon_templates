89e27a296d8735f64319aa9f16097901d0edb837
KoynovStas <skojnov@yandex.ru>
2015-05-27 15:27:37 +0300

    Add template_2

---
+++ template_2/daemon2.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
+
+#include "daemon.h"
+
+
+
+
+
+int main(void)
+{
+
+    daemonize();
+
+
+
+    while(1)
+    {
+
+        // Here а routine of daemon
+
+        printf("%s: daemon is run\n", DAEMON_NAME);
+        sleep(10);
+    }
+
+
+
+    return EXIT_SUCCESS; // good job (we interrupted (finished) main loop)
+}
