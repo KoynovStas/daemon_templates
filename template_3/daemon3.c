2d538c45492e137c84f2ba846d5c8d1a4631ac68
KoynovStas <skojnov@yandex.ru>
2015-05-27 15:29:49 +0300

    Add template_3

---
+++ template_3/daemon3.c
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+
+
+#include "daemon.h"
+
+
+
+
+
+void daemon_exit_handler(int sig)
+{
+
+    //Here we release resources
+
+
+
+#ifdef  DAEMON_PID_FILE_NAME
+    unlink(DAEMON_PID_FILE_NAME);
+#endif
+
+
+    _exit(EXIT_FAILURE);
+}
+
+
+
+void init_signals(void)
+{
+
+    struct sigaction sa;
+
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = daemon_exit_handler;
+    if( sigaction(SIGTERM, &sa, NULL) != 0)
+        daemon_error_exit("Can't set daemon_exit_handler: %m\n");
+
+
+
+    signal(SIGCHLD, SIG_IGN); // ignore child
+    signal(SIGTSTP, SIG_IGN); // ignore tty signals
+    signal(SIGTTOU, SIG_IGN);
+    signal(SIGTTIN, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+}
+
+
+
+int main(void)
+{
+
+    daemonize();
+    init_signals();
+
+
+
+    while(1)
+    {
+
+        // Here а routine of daemon
+
+        printf("%s: daemon is run\n", DAEMON_NAME);
+        sleep(10);
+    }
+
+
+
+    return EXIT_SUCCESS; // good job (we interrupted (finished) main loop)
+}
